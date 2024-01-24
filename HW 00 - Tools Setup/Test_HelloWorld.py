e6fcc61878366425a9d675ead5a170bf671608ef
SHASHANKJC <84567001+SHASHANKJC@users.noreply.github.com>
2024-01-24 09:25:26 -0500

    Add files via upload

---
+++ HW 00 - Tools Setup/Test_HelloWorld.py
@@ -0,0 +1,13 @@
+import unittest
+from HelloWorld import hello_world
+
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello, World!")
+
+
+if __name__ == '__main__':
+    unittest.main()
+unittest.main()
