cb2de996809632fe41cfbbac0a354e8eb00aa130
SHASHANKJC <84567001+SHASHANKJC@users.noreply.github.com>
2024-02-23 23:03:22 -0500

    Update hw04.py

--- HW4/hw04.py
+++ HW4/hw04.py
@@ -49,11 +49,3 @@ def get_user_repositories(user_id):
                 print(f'Error fetching commits for repository {repo_name}')
     else:
         print(f'Error fetching repositories for user {user_id}')
-
-# test by running this and let me know any changes is required.
-# we can add a loop here to check for unlimited repos until user wants exit
-# let me know if any changes is required, I'll be available
-
-
-get_user_id = input('Enter the user_id you would like to: ')
-get_user_repositories(get_user_id)
